ac4a549f1cb7273a9789dee028e507a0fbb321ec
Dwight <dovalascho@gmail.com>
2022-12-14 19:44:10 -0500

    Added savePreferences, manual time fixed

--- app/src/main/java/com/example/timeadder/MainActivity.java
+++ app/src/main/java/com/example/timeadder/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.timeadder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
